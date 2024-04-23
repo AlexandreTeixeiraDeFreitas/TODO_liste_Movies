98709d6b5c10ae254d3c62a34c87197cbe3b6d70
AlexandreTeixeiraDeFreitas <75750185+AlexandreTeixeiraDeFreitas@users.noreply.github.com>
2024-04-23 21:12:03 +0200

    User

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -6,7 +6,8 @@ const router = Router();
 
 router.post('/login', passport.authenticate('local', { session: false }), UserController.login);
 router.post("/", passport.authenticate('jwt-user?', { session: false }), UserController.createUser);
-router.get("/", passport.authenticate('jwt-admin', { session: false }), UserController.getAllUsers);
+router.get("/", passport.authenticate('jwt-user', { session: false }), UserController.getUser);
+router.get("/getAllUsers", passport.authenticate('jwt-admin', { session: false }), UserController.getAllUsers);
 router.get("/:id", passport.authenticate('jwt-user', { session: false }), UserController.getUserById);
 router.put("/:id", passport.authenticate('jwt-user', { session: false }), UserController.updateUser);
 router.delete("/:id", passport.authenticate('jwt-user', { session: false }), UserController.deleteUser);
